b7dff976c3f99f9151e51e0e88699d43cccb719e
Unknown <addisonkevin0@gmail.com>
2018-05-20 09:18:53 +0100

    Project Setup

---
+++ src/main/java/com/sky/library/service/BookService.java
@@ -0,0 +1,14 @@
+package com.sky.library.service;
+
+import com.sky.library.exception.BookNotFoundException;
+import com.sky.library.obj.Book;
+
+/*
+ * Copyright © 2015 Sky plc All Rights reserved.
+ * Please do not make your solution publicly available in any way e.g. post in forums or commit to GitHub.
+ */
+
+public interface BookService {
+    Book retrieveBook(String bookReference) throws BookNotFoundException;
+    String getBookSummary(String bookReference) throws BookNotFoundException;
+}
